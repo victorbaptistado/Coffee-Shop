9819cc317b06e1235aea4f0c063b2264ddaf2ae6
victorbaptistado <victorbaptistado@gmail.com>
2022-02-09 18:07:06 +0000

    Mobile upgrade

--- myApp.js
+++ myApp.js
@@ -4,13 +4,13 @@ var body = document.getElementsByTagName("body");
 
 
 function myFunction(mobile) {
-  if (mobile.matches ) { // If media query matches
-    console.log("Good");
+  if (mobile.matches) { // If media query matches
+    document.getElementsByClassName;
   } else {
     document.body.style.backgroundColor = "pink";
   }
 }
 
-var mobile = window.matchMedia("(max-width: 700px)")
+var mobile = window.matchMedia("(max-width: 765px)")
 myFunction(mobile) // Call listener function at run time
 mobile.addListener(myFunction) // Attach listener function on state changes
