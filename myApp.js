22bb2fcefe21369069b5983839e212a17e24e323
victorbaptistado <victorbaptistado@gmail.com>
2022-02-15 10:39:05 +0000

    Upgrade

--- myApp.js
+++ myApp.js
@@ -1,4 +1,4 @@
-/*var body = document.getElementsByTagName("body");*//*
+/*var body = document.getElementsByTagName("body");*/ /*
 var bodyId = document.querySelector(".body").addEventListener("click", hideDiv)*/
 /*
 function myFunction(mobile) {
@@ -14,34 +14,27 @@ myFunction(mobile) // Call listener function at run time
 mobile.addListener(myFunction) // Attach listener function on state changes
 */
 
-
-
 /*var body = document.getElementsById("body");*/
 var mobileMenu = document.getElementById("mobileMenu");
 var mobileNavigation = document.getElementById("mobileNavigation");
 var closeMenu = document.getElementById("closeMenu");
 /*var backgroundHeader = document.querySelector(".backgroundHeader").addEventListener("click", hideDiv);*/
-var backgroundBody = document.querySelector(".backgroundBody").addEventListener("click", hideDiv);
-var imageHeader = document.querySelector(".ourMenusImage").addEventListener("click", hideDiv);
+
+
 var headerText = document.querySelector(".headerText");
-
-
 
 mobileMenu.addEventListener("click", displayDiv);
 
-function displayDiv (){
+function displayDiv() {
   mobileNavigation.style.display = "block";
   mobileMenu.style.display = "none";
   closeMenu.style.display = "block";
-
-
 }
 
 closeMenu.addEventListener("click", hideDiv);
 
-function hideDiv(){
+function hideDiv() {
   mobileNavigation.style.display = "none";
   mobileMenu.style.display = "block";
   closeMenu.style.display = "none";
- }
- 
+}
